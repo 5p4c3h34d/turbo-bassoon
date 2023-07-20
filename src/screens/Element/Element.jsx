194bd81e5a9a9142ac4c5e20e415013723b84f8a
5p4c3h34d <140087513+5p4c3h34d@users.noreply.github.com>
2023-07-20 18:58:52 +0400

    import

--- src/screens/Element/Element.jsx
+++ src/screens/Element/Element.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useRef } from 'react';
 import { Button } from "../../components/Button";
 import { ContactUs } from "../../components/ContactUs";
 import { PropertyDefaultWrapper } from "../../components/PropertyDefaultWrapper";
